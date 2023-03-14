38e4f74f431429416dca32d526da9e4ed0cf209d
yukang <18790336164@163.com>
2023-03-14 12:44:53 +0800

    添加配置文件

---
+++ src/mixin/add-edit-form.js
@@ -0,0 +1,143 @@
+import { cloneDeep } from "lodash";
+import valid from "@/utils/valid";
+export const addEditForm = {
+  props: {
+    visible: {
+      type: Boolean,
+      default: false,
+    },
+    type: {
+      type: Number,
+      default: 1,
+    },
+    options: { type: Object, default: () => ({}) },
+  },
+  computed: {
+    getTitle() {
+      return this.type ? this.titleMap[this.type] + this.title : "详情";
+    },
+    getAddOrEdit() {
+      return [1, 2].includes(this.type);
+    },
+    getIsUpdate() {
+      return this.type === 2;
+    },
+    getIsView() {
+      return this.type === 3;
+    },
+    getIsAdd() {
+      return this.type === 1;
+    },
+  },
+  watch: {
+    visible(val) {
+      this.dialogFormVisible = val;
+    },
+  },
+  data() {
+    return {
+      dialogFormVisible: this.visible,
+      copyFormDesc: {},
+      formData: {},
+      order: [],
+      titleMap: {
+        1: "添加",
+        2: "修改",
+        3: "查看",
+      },
+    };
+  },
+  methods: {
+    ...valid,
+    async fetchSubmitResult() {
+      this.$modal.msgSuccess(`${this.titleMap[this.type]}成功`);
+      this.handleClosed();
+      this.$emit("success");
+    },
+    handleClosed() {
+      this.$emit("update:visible", false);
+      this.formData = {};
+      this.formDesc = cloneDeep(this.copyFormDesc || {});
+      this.order = [];
+    },
+    setTimeRange(start, end) {
+      return this.type === 3
+        ? `从${start}，至${end}`
+        : [new Date(start), new Date(end)];
+    },
+    async initViewData() {
+      for (const key in this.options.viewFormDesc) {
+        if (this.formDesc[key]) {
+          delete this.options.viewFormDesc[key];
+        }
+      }
+      this.formDesc = { ...this.formDesc, ...this.options.viewFormDesc };
+      for (const key in this.formDesc) {
+        const { labelWidth, layoutMax, layout, type, ...rest } =
+          this.formDesc[key];
+        this.formDesc[key] = {
+          ...this.formDesc[key],
+          type: ["json-editor", "gallery"].includes(type) ? type : "text",
+          required: false,
+          layout: labelWidth === "0" || layoutMax ? 24 : 12,
+        };
+      }
+      const noOrder = [];
+      const order = [];
+      Object.entries(this.formDesc).map((item) => {
+        item[1].order
+          ? order.push({ ...item[1], field: item[0] })
+          : noOrder.push(item[0]);
+      });
+      const arrField = order
+        .sort((a, b) => a.order - b.order)
+        .map((item) => item.field);
+      this.order = [...noOrder, ...arrField];
+    },
+    async initFormDesc() {
+      if (!Object.keys(this.copyFormDesc).length && this.formDesc) {
+        for (const key in this.formDesc) {
+          const item = this.formDesc[key];
+          item.attrs = {
+            ...(item.attrs || {}),
+            clearable: "clearable" in item ? item.clearable : true,
+          };
+          this.$set(this.formDesc[key], "attrs", item.attrs);
+        }
+        this.copyFormDesc = cloneDeep(this.formDesc || {});
+      } else {
+        this.formDesc = cloneDeep(this.copyFormDesc || {});
+      }
+
+      const isView = this.type === 3;
+
+      if (isView) {
+        await this.initViewData();
+      }
+
+      if (this.gridOptions) {
+        if ("columns" in this.gridOptions) {
+          const index = this.gridOptions.columns.findIndex(
+            (item) => item.type === "checkbox"
+          );
+          const indexOption = this.gridOptions.columns[index];
+          if (indexOption) {
+            indexOption.visible = !isView;
+            this.gridOptions.columns.splice(index, 1, indexOption);
+          }
+        }
+
+        this.gridOptions.editConfig.enabled = [1, 2].includes(this.type);
+        this.gridOptions.toolbarConfig.buttons.map((item) => {
+          item.visible = !isView;
+        });
+      }
+
+      Object.entries(this.formDesc).map((item) => {
+        if ("default" in item[1] && !(item[0] in this.formData)) {
+          this.formData[item[0]] = item[1].default;
+        }
+      });
+    },
+  },
+};
---
+++ src/mixin/grid-table.js
@@ -0,0 +1,291 @@
+import { mapState } from "vuex";
+import { checkPermi } from "@/utils/permission";
+import gridOptions from "@/utils/grid-options";
+import { camel2snake } from "@/utils/camel-case";
+import { isFunction } from "@/utils/validate";
+import { cloneDeep } from "lodash";
+
+const searchButton = {
+  span: 24,
+  align: "center",
+  collapseNode: true,
+  itemRender: {
+    name: "$buttons",
+    children: [
+      {
+        props: {
+          type: "submit",
+          content: "app.body.label.search",
+          status: "primary",
+          icon: "vxe-icon-search",
+        },
+      },
+      {
+        props: {
+          type: "reset",
+          content: "app.body.label.reset",
+          icon: "vxe-icon-refresh",
+        },
+      },
+    ],
+  },
+};
+
+export const gridTable = {
+  data() {
+    return {
+      gridTableData: [],
+      exportTotal: 0,
+      gridOptions,
+      addEditFormDialog: {
+        open: false,
+        options: {},
+        operationType: 1,
+      },
+    };
+  },
+  computed: {
+    ...mapState([]),
+  },
+  created() {
+    this.initGrid();
+  },
+  methods: {
+    initGrid() {
+      this.checkToolbarConfigPermi();
+      if (this.gridOptions.proxyConfig.autoLoad !== false) {
+        this.gridOptions.proxyConfig.ajax = {
+          query: async ({ page, form }) => {
+            const params = isFunction(this.workFormParams)
+              ? this.workFormParams({
+                  ...form,
+                  ...page,
+                })
+              : form;
+            const res = await this.fetch.getList({
+              ...params,
+              pageNum: page.currentPage,
+              pageSize: page.pageSize,
+            });
+            this.exportTotal = res.total;
+            this.gridTableData = res.rows;
+            return res;
+          },
+        };
+      } else {
+        delete this.gridOptions.proxyConfig;
+      }
+
+      let items = this.gridOptions?.formConfig?.items;
+      if (items.length) {
+        const searchBtn = cloneDeep(searchButton);
+        if (items.length < 3) {
+          searchBtn.span = 8;
+          searchBtn.align = "left";
+        }
+
+        if (items.length <= 4) {
+          searchBtn.collapseNode = false;
+        }
+
+        items.map((item, index) => {
+          const { name, props } = item.itemRender || {};
+          item.folding = index > (items.length <= 3 ? 2 : 3);
+          item.span = items.length <= 3 ? 8 : 6;
+          switch (name) {
+            case "$select":
+            case "$input":
+              item.itemRender.props = {
+                placeholder: `请${name === "$input" ? "输入" : "选择"}${
+                  item.title
+                }`,
+                clearable:
+                  "clearable" in item.itemRender
+                    ? item.itemRender.clearable
+                    : true,
+                ...props,
+              };
+          }
+        });
+
+        this.gridOptions.formConfig.items.push(searchBtn);
+      }
+
+      if (!this.gridOptions.noSortable) {
+        this.gridOptions.columns = this.gridOptions.columns.map((item) => {
+          if (!("sortable" in item)) {
+            item.sortable = !["操作", "备注"].includes(item.title);
+          }
+          return item;
+        });
+      }
+    },
+    handleFormReset({ data }) {
+      const values = Object.values(data).filter((item) => !!item);
+      if (!values.length) {
+        return;
+      }
+      for (const key in data) {
+        data[key] = null;
+      }
+      this.fetchGiridData();
+    },
+    setFormConfigSelectItemOptions(name, dictName = null) {
+      const index = this.gridOptions.formConfig.items.findIndex(
+        (item) => item.field === name
+      );
+      this.gridOptions.formConfig.items[index].itemRender.options =
+        this.dict.type[dictName ? dictName : camel2snake(name)];
+    },
+    setFormConfigSelectItemValue(name, value) {
+      const index = this.gridOptions.formConfig.items.findIndex(
+        (item) => item.field === name
+      );
+      this.gridOptions.formConfig.items[index].itemRender.options = value;
+    },
+    checkToolbarConfigPermi() {
+      let { buttons, tools } = this.gridOptions.toolbarConfig;
+      const CheckPermi = (arr) =>
+        (arr || []).map((item) => {
+          if (item.permi) {
+            item.visible = checkPermi(item.permi);
+          }
+          return item;
+        });
+      // 校验左侧增删改查是否有操作权限
+      buttons && (buttons = CheckPermi(buttons));
+      // 校验右侧配置的按钮是否有操作权限
+      tools && (buttons = CheckPermi(tools));
+    },
+    async handleToolbarButtonClick({ code, $grid }) {
+      const rows = $grid.getCheckboxRecords();
+      switch (code) {
+        case "add":
+          this.handleAdd();
+          break;
+        case "update":
+          if (!rows.length) {
+            this.$message({
+              message: "请选择要修改的数据",
+              type: "warning",
+            });
+            return;
+          }
+
+          if (rows.length > 1) {
+            this.$message({
+              message: "只能选择一条进行修改",
+              type: "warning",
+            });
+            return;
+          }
+          this.handleUpdate(rows[0]);
+          break;
+        case "del":
+          if (!rows.length) {
+            this.$message({
+              message: "请选择要删除的数据",
+              type: "warning",
+            });
+            return;
+          }
+          await this.$modal.confirm(
+            `是否确认删除${rows.length > 1 ? "所选" : "改行"}数据？`
+          );
+          this.handleDelete($grid.getCheckboxRecords());
+          break;
+        default:
+          this.handleClickToolbarButton &&
+            this.handleClickToolbarButton({ code, $grid });
+      }
+    },
+    async handleToolbarToolClick({ tool, code, $grid }) {
+      switch (code) {
+        case "download":
+          const { form, pager } = $grid.getProxyInfo();
+          this.handleExport(
+            {
+              ...form,
+              pageNum: pager.currentPage,
+              pageSize: this.exportTotal || pager.pageSize,
+            },
+            tool
+          );
+          break;
+        default:
+          this.handleClickToolbarTool &&
+            this.handleClickToolbarTool({ code, $grid });
+      }
+    },
+    fetchGiridData() {
+      this.$refs.xGrid.commitProxy("query");
+    },
+    handleAdd() {
+      this.addEditFormDialog.operationType = 1;
+      this.addEditFormDialog.options = {};
+      this.addEditFormDialog.open = true;
+      this.addEditFormDialog.view = false;
+    },
+    getViewFormDesc() {
+      return this.gridOptions.columns
+        .filter((item) => !["", "操作"].includes(item.title))
+        .reduce((pre, curr) => {
+          const { slots, ...rest } = curr;
+          pre[curr.field] = {
+            ...rest,
+            type: curr.type || "text",
+            label: curr.title,
+            layout: 12,
+          };
+
+          if (
+            curr.slots &&
+            !curr.slots.default.toUpperCase().includes("TIME") &&
+            Array.isArray(this.dict.type[curr.slots.default])
+          ) {
+            pre[curr.field].options = this.dict.type[curr.slots.default].map(
+              (item) => ({
+                text: item.label,
+                value: Number(item.value),
+              })
+            );
+          }
+          return pre;
+        }, {});
+    },
+    async handleUpdate(row, type = 2) {
+      const { data } = await this.fetch.getRowData(row[this.fetch.id]);
+      this.addEditFormDialog.options = data;
+      if (type === 3) {
+        this.addEditFormDialog.options.viewFormDesc = this.getViewFormDesc();
+      }
+      this.addEditFormDialog.operationType = type;
+      this.addEditFormDialog.open = true;
+    },
+    async handleView(row) {
+      this.handleUpdate(row, 3);
+    },
+    async handleDelete(row) {
+      const ids = Array.isArray(row)
+        ? row.map((item) => item[this.fetch.id])
+        : row[this.fetch.id];
+      await this.fetch.delete(ids);
+      this.$refs.xGrid.remove(row);
+      this.$modal.msgSuccess("删除成功");
+    },
+    // 获取字典的键值对
+    getStatusMap(type) {
+      return this.dict.type[type].reduce((prev, next) => {
+        prev[next.value] = next.label;
+        return prev;
+      }, {});
+    },
+    handleExport(params, tool) {
+      this.download(
+        tool.url,
+        params,
+        `${tool.url.split("/")[1]}_${new Date().getTime()}.xlsx`
+      );
+    },
+  },
+};
