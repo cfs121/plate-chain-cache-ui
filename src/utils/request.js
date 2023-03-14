6d2520e826814ec9fb4de25c09cd4eb01697cc51
Doug Wang <wangtao@deehero.com>
2023-03-14 13:24:12 +0800

    Initial commit

--- src/utils/request.js
+++ src/utils/request.js
@@ -108,12 +108,15 @@ service.interceptors.response.use(res => {
 }, error => {
   console.log('err' + error)
   let { message } = error
-  if (message == 'Network Error') {
+
+  const status = error.response.status
+  if(status === 401) {
+    message = error.response.data.body;
+  }else if (message == 'Network Error') {
     message = '后端接口连接异常'
   } else if (message.includes('timeout')) {
     message = '系统接口请求超时'
   } else if (message.includes('Request failed with status code')) {
-
     message = '系统接口' + message.substr(message.length - 3) + '异常'
   }
   Message({ message: message, type: 'error', duration: 5 * 1000 })
