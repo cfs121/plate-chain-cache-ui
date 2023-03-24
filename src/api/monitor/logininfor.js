0410c63b2b289e90111ce0e22312b548254f7466
tongchuan <905704187@qq.com>
2023-03-24 16:11:54 +0800

    日志管理模块功能修改完成（还有时间重置与解锁问题）

--- src/api/monitor/logininfor.js
+++ src/api/monitor/logininfor.js
@@ -3,32 +3,41 @@ import request from '@/utils/request'
 // 查询登录日志列表
 export function list(query) {
   return request({
-    url: '/monitor/logininfor/list',
+    url: '/monitor_loginlog/page',
     method: 'get',
-    params: query
+    params: {
+      pageNumber: query.pageNumber,
+      pageSize: query.pageSize,
+      Q_EQ_ipAddr: query.ipAddr,
+      Q_EQ_userName: query.userName,
+      Q_EQ_status: query.status,
+      Q_BT_loginTime: query.dataRange
+    }
   })
 }
 
 // 删除登录日志
-export function delLogininfor(infoId) {
+export function delLoginLog(ids) {
   return request({
-    url: '/monitor/logininfor/' + infoId,
-    method: 'delete'
+    url: '/monitor_loginlog/',
+    method: 'delete',
+    data:ids
   })
 }
+//
 
 // 解锁用户登录状态
-export function unlockLogininfor(userName) {
+export function unlockLoginLog(userName) {
   return request({
-    url: '/monitor/logininfor/unlock/' + userName,
+    url: '/monitor_loginlog/unlock/' + userName,
     method: 'get'
   })
 }
 
 // 清空登录日志
-export function cleanLogininfor() {
+export function cleanLoginLog() {
   return request({
-    url: '/monitor/logininfor/clean',
+    url: '/monitor_loginlog/clean',
     method: 'delete'
   })
 }
