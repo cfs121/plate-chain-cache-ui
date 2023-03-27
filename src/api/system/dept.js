7ac75dfab6df9f8568783a7af4736303d81477a5
chensheng <1971630084@qq.com>
2023-03-27 10:19:13 +0800

    查询列表的URL由/sys-dept/page改成/sys-dept/list

--- src/api/system/dept.js
+++ src/api/system/dept.js
@@ -3,24 +3,26 @@ import request from '@/utils/request'
 // 查询部门列表
 export function listDept(query) {
   return request({
-    url: '/sys-dept/page',
+    url: '/sys-dept/list',
     method: 'get',
     params: {
-      pageNumber: 1,
-      pageSize: 1000,
+
       Q_LIKE_deptName: query.deptName,
       Q_EQ_status: query.status
     }
   })
 }
 
+
+
 // 查询部门列表（排除节点）
 export function listDeptExcludeChild(id) {
   return request({
-    url: '/sys-dept/list/exclude/' + id,
+    url: '/sys-dept/exclude/' + id,
     method: 'get'
   })
 }
+
 
 // 查询部门详细
 export function getDept(id) {
@@ -29,6 +31,8 @@ export function getDept(id) {
     method: 'get'
   })
 }
+
+
 
 // 新增部门
 export function addDept(data) {
@@ -54,4 +58,7 @@ export function delDept(id) {
     url: '/sys-dept/' + id,
     method: 'delete'
   })
+
+
+
 }
